c420c52af682f5ea3e9987280f3700078d36466f
Daniel Spady <danielspadyassurance22@gmail.com>
2025-11-13 17:53:26 -0500

    Initial Commit

---
+++ AllTrailsMapSearchDemoStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AllTrailsMapSearchDemoStoryboard
+//
+//  Created by Daniel Spady on 11/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
